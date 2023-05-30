6d746edf2778088b6ba529e29006bffb253bd4cb
Nate Patterson <nathaniellawrencepatterson@gmail.com>
2023-05-30 12:43:11 -0400

    Changed __init__.py

--- src/personalities/__init__.py
+++ src/personalities/__init__.py
@@ -1,9 +1,11 @@
 from .luna import luna
 from .sacha import sacha
+from sandra.py import sandra
 
 __all__ = [
     "sacha",
     "luna",
+    "sandra",
     "get_personality"
 ]
 
@@ -12,7 +14,8 @@ def get_personality(name: str):
     try:
         return {
             "luna": luna,
-            "sacha": sacha
+            "sacha": sacha,
+            "sandra": sandra
         }[name]
     except Exception:
         raise Exception("The personality you selected does not exist!")
