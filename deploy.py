24ec30ad26fe46e884152d4d57cbdf933280db58
Enias Cailliau <enias@steamship.com>
2023-05-31 17:10:32 +0200

    Make deploy script more reliable

--- deploy.py
+++ deploy.py
@@ -8,12 +8,11 @@ from steamship.data.manifest import Manifest
 try:
     deploy()
 except SystemExit as err:
-    pass
+    exit()
 
 manifest = Manifest.load_manifest()
 
 client = Steamship(workspace=f"girlfriend-ai-{uuid.uuid1()}")
-
 
 bot = client.use(
     package_handle=manifest.handle,
@@ -21,7 +20,7 @@ bot = client.use(
     instance_handle=f"{manifest.handle}-{manifest.version.replace('.', '-')}",
     config={
         "bot_token": os.environ.get("BOT_TOKEN")
-        or input(
+                     or input(
             "Paste your telegram bot token\nLearn how to create one here: https://github.com/EniasCailliau/GirlfriendGPT/blob/main/docs/register-telegram-bot.md)\n: "
         ),
         "elevenlabs_voice_id": os.environ.get("ELEVENLABS_VOICE_ID", ""),
@@ -33,7 +32,7 @@ bot.wait_for_init()
 print(client.config.workspace_handle)
 print(bot.package_version_handle)
 print(
-    f"""Chat with your bot here: 
+    f"""Chat with your bot here:
 
 https://www.steamship.com/workspaces/{client.config.workspace_handle}/packages/{bot.handle}"""
 )
