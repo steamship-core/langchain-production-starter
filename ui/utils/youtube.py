25adf1fb286ce6e7438547b1976784b9429f9d89
Enias Cailliau <enias@steamship.com>
2023-07-08 00:14:11 +0200

    Add back-up to Youtube app api

--- ui/utils/youtube.py
+++ ui/utils/youtube.py
@@ -11,27 +11,38 @@ def download_html(url):
     return html
 
 
-def extract_channel_id(channel_url):
-    html_content = download_html(channel_url)
-    pattern = r'<link rel="canonical" href="https://www.youtube.com/channel/(.*?)"'
-    matches = re.findall(pattern, html_content)
-    return matches[0] if matches else None
+def extract_channel_id_and_name(html_content):
+    id_pattern = r'<link rel="canonical" href="https://www.youtube.com/channel/(.*?)"'
+    title_pattern = r'<title>(.*?) - YouTube</title>'
+
+    id_match = re.findall(id_pattern, html_content)
+    title_match = re.findall(title_pattern, html_content)
+
+    channel_id = id_match[0] if id_match else None
+    channel_name = title_match[0] if title_match else None
+    return channel_id, channel_name
 
 
 def get_channel_details(channel_url):
     api_key = st.secrets["google_api_key"]
     youtube = build("youtube", "v3", developerKey=api_key)
 
-    channel_id = extract_channel_id(channel_url)
-    print("channel_id", channel_id)
+    html_content = download_html(channel_url)
+    channel_id, channel_name = extract_channel_id_and_name(html_content)
+    channel_profile_pic = None
     if not channel_id:
         raise Exception("Could not find channel_id.")
-    request = youtube.channels().list(part="snippet", id=channel_id)
-    response = request.execute()
+    try:
+        request = youtube.channels().list(part="snippet", id=channel_id)
+        response = request.execute()
 
-    items = response.get("items", [{}])[0]
-    snippet = items.get("snippet", {})
+        items = response.get("items", [{}])[0]
+        snippet = items.get("snippet", {})
+        channel_name = snippet.get("title")
+        channel_profile_pic = snippet.get("thumbnails", {}).get("default", {}).get("url")
+    except Exception:
+        pass
+    return channel_name, channel_profile_pic
 
-    return snippet.get("title"), snippet.get("thumbnails", {}).get("default", {}).get(
-        "url"
-    )
+
+print(get_channel_details("https://www.youtube.com/@AlexHormozi"))
